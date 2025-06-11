15f562905d6b5e5add8d83a0fe42e7f29dc839e4
Sayan-sam <sayanb1729@gmail.com>
2025-06-11 08:42:56 +0000

    Handle repeat error
    Signed-off-by: Sayan-sam <sayanb1729@gmail.com>

--- src/engine_init.py
+++ src/engine_init.py
@@ -3,7 +3,10 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 engine = create_engine("sqlite:///corporate.db")
-pd.read_csv("data/external_csv/Corporate_Actions.csv").to_sql("corporate_action", engine, index=False)
+try:
+    pd.read_csv("data/external_csv/Corporate_Actions.csv").to_sql("corporate_action", engine, index=False)
+except ValueError as e:
+    print(e)
 
 
 db = SQLDatabase(engine=engine)
